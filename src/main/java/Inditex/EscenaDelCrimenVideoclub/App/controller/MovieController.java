2ab1907db2a04b51af809bf0e4f7a04a50aa75dd
webermuros1 <webermuros1@gmail.com>
2026-04-06 16:22:26 +0200

    fix:Clean code and corrections to make it look like the professor's

--- src/main/java/Inditex/EscenaDelCrimenVideoclub/App/controller/MovieController.java
+++ src/main/java/Inditex/EscenaDelCrimenVideoclub/App/controller/MovieController.java
@@ -4,6 +4,10 @@ package Inditex.EscenaDelCrimenVideoclub.App.controller;
 import Inditex.EscenaDelCrimenVideoclub.App.model.Movie;
 import org.springframework.web.bind.annotation.*;
 import Inditex.EscenaDelCrimenVideoclub.App.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.Optional;
+
 
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:5173")
@@ -17,31 +21,48 @@ public class MovieController {
     }
 
     @GetMapping("/movies")
-    public List<Movie> getAllMovie(){
+    public List<Movie> getAllMovie() {
         return movieService.getAll();
     }
 
     @PostMapping("/movies")
-    public Movie createMovie(@RequestBody Movie newMovie){
+    public Movie createMovie(@RequestBody Movie newMovie) {
         return movieService.addMovie(newMovie);
     }
 
     @DeleteMapping("/movies/{id}")
-    public void deleteMovieById(@PathVariable int id){
+    public void deleteMovieById(@PathVariable int id) {
         movieService.deleteMovie(id);
     }
 
     @PutMapping("/movies/{id}")
-    public Movie updateMovie(@PathVariable int id, @RequestBody Movie updatedMovie){
-        return movieService.updateMovie(id, updatedMovie);
+    public ResponseEntity<Movie> updateMovie(@PathVariable int id, @RequestBody Movie updatedMovie){
+        try{
+            Movie movie = movieService.updateMovie(id, updatedMovie);
+            return new ResponseEntity<>(movie, HttpStatus.OK);
+        }
+        catch(Exception exception){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
+
     @GetMapping("/movies/ASC")
-    public List<Movie> getAllByOrder(){
+    public List<Movie> getAllByOrder() {
         return movieService.getAllByOrder();
     }
 
     @GetMapping("/movies/category/{category}")
-    public List<Movie> getByCategory(@PathVariable String category){
+    public List<Movie> getByCategory(@PathVariable String category) {
         return movieService.getByCategory(category);
+    }
+
+    @GetMapping("/movies/{id}")
+    public ResponseEntity<Movie> findMovieById(@PathVariable int id) {
+        Optional<Movie> foundMovie = movieService.findMovie(id);
+        if (foundMovie.isPresent()) {
+            return new ResponseEntity<>(foundMovie.get(), HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/Inditex/EscenaDelCrimenVideoclub/App/service/MovieService.java
+++ src/main/java/Inditex/EscenaDelCrimenVideoclub/App/service/MovieService.java
@@ -4,9 +4,11 @@ import Inditex.EscenaDelCrimenVideoclub.App.model.Movie;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import Inditex.EscenaDelCrimenVideoclub.App.repository.MovieRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MovieService {
     private final MovieRepository movieRepository;
@@ -27,13 +29,42 @@ public class MovieService {
     public void deleteMovie(int id){
         movieRepository.deleteById(id);
     }
+    public Optional<Movie> findMovie(int id){
+        return movieRepository.findById(id);
+    }
 
     public Movie updateMovie(int id, Movie updatedMovie){
-        updatedMovie.setId(id);
-        return movieRepository.save(updatedMovie);
+
+        //verificamos que aun existe la pelicula despues de rellenar el formulario
+        Optional<Movie> foundMovie = movieRepository.findById(id);
+
+        if(foundMovie.isPresent()){
+            Movie existingMovie = foundMovie.get();
+
+            //Actualizamos los campos
+            existingMovie.setTitle(updatedMovie.getTitle());
+            existingMovie.setYear(updatedMovie.getYear());
+            existingMovie.setDirector(updatedMovie.getDirector());
+            existingMovie.setMain_cast(updatedMovie.getMain_cast());
+            existingMovie.setFilmaffinity_score(updatedMovie.getFilmaffinity_score());
+            existingMovie.setDescription(updatedMovie.getDescription());
+            existingMovie.setImg(updatedMovie.getImg());
+            existingMovie.setUrl(updatedMovie.getUrl());
+            existingMovie.setTrailer(updatedMovie.getTrailer());
+            existingMovie.setRanking(updatedMovie.getRanking());
+            existingMovie.setCategory(updatedMovie.getCategory());
+
+            //guardar la pelicula
+            return movieRepository.save(existingMovie);
+        }
+
+        //si no existe la pelicula, enviar un mensaje de error al usuario
+        throw new RuntimeException("Movie not found in DDBB with id : " + id);
     }
+
+
     public List<Movie> getAllByOrder(){
-        return movieRepository.findAll(Sort.by(Sort.Direction.ASC, "title")).reversed();
+        return movieRepository.findAll(Sort.by(Sort.Direction.ASC, "title"));
     }
 
     public List<Movie> getByCategory(String category){
