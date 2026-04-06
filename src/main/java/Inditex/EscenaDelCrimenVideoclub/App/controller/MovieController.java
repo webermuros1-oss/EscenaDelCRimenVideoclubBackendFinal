6d6e1b6047c3edae254d1814343855dd72954239
webermuros1 <webermuros1@gmail.com>
2026-04-06 12:22:24 +0200

    feat:MovieController

---
+++ src/main/java/Inditex/EscenaDelCrimenVideoclub/App/controller/MovieController.java
@@ -0,0 +1,47 @@
+package Inditex.EscenaDelCrimenVideoclub.App.controller;
+
+
+import Inditex.EscenaDelCrimenVideoclub.model.Movie;
+import org.springframework.web.bind.annotation.*;
+import Inditex.EscenaDelCrimenVideoclub.service.MovieService;
+
+import java.util.List;
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAllMovie(){
+        return movieService.getAll();
+    }
+
+    @PostMapping("/movies")
+    public Movie createMovie(@RequestBody Movie newMovie){
+        return movieService.addMovie(newMovie);
+    }
+
+    @DeleteMapping("/movies/{id}")
+    public void deleteMovieById(@PathVariable int id){
+        movieService.deleteMovie(id);
+    }
+
+    @PutMapping("/movies/{id}")
+    public Movie updateMovie(@PathVariable int id, @RequestBody Movie updatedMovie){
+        return movieService.updateMovie(id, updatedMovie);
+    }
+    @GetMapping("/movies/ASC")
+    public List<Movie> getAllByOrder(){
+        return movieService.getAllByOrder();
+    }
+
+    @GetMapping("/movies/category/{category}")
+    public List<Movie> getByCategory(@PathVariable String category){
+        return movieService.getByCategory(category);
+    }
+}
